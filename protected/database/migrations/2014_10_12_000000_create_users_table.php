4f9bbd2f15e20b1a7edc2455fdaa9ce262284dc4
chriss-inno <chriss.innocent@outlook.com>
2016-06-16 00:37:02 +0300

    first commit

---
+++ protected/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->string('department_id');
+            $table->string('campus_id');
+            $table->string('designation');
+            $table->string('status');
+            $table->string('locked');
+            $table->string('user_name')->unique();
+            $table->string('password', 100);
+            $table->dateTime('last_login')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
